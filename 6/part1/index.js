1b0f86092db797b5a59bf666347a86dfc1f01a24
Shane Vroegop <wizzy0309@hotmail.com>
2022-12-27 00:12:08 +1000

    Day 6 Part 2

--- 6/part1/index.js
+++ 6/part1/index.js
@@ -4,32 +4,35 @@ import { fileURLToPath } from 'url';
 
 const UNIQUE_CHARS_NEEDED = 4;
 
+export function getMessages(input, charsNeeded) {
+    for(let i = 0; i < input.length; i++) {
+        let uniqueCharCount = 0;
+        const tempChars = input.slice(i, i + charsNeeded);
+        for(let j = 0; j < tempChars.length; j++ ) {
+            let occurrences = 0;
+            for(let k = 0; k < tempChars.length; k++) {
+                if(tempChars[j] === tempChars[k]) {
+                    occurrences++;
+                }
+            }
+            if(occurrences === 1) {
+                uniqueCharCount++;
+            }
+        }
+        if(uniqueCharCount === charsNeeded) {
+            console.log(i + charsNeeded);
+            return;
+        }
+    }
+}
+
 function solve() {
     try {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = dirname(__filename);
 
         const input = fs.readFileSync(__dirname + "/../input.txt", 'utf8');
-
-        for(let i = 0; i < input.length; i++) {
-            let uniqueCharCount = 0;
-            const tempChars = input.slice(i, i + UNIQUE_CHARS_NEEDED);
-            for(let j = 0; j < tempChars.length; j++ ) {
-                let occurrences = 0;
-                for(let k = 0; k < tempChars.length; k++) {
-                    if(tempChars[j] === tempChars[k]) {
-                        occurrences++;
-                    }
-                }
-                if(occurrences === 1) {
-                    uniqueCharCount++;
-                }
-            }
-            if(uniqueCharCount === UNIQUE_CHARS_NEEDED) {
-                console.log(i + UNIQUE_CHARS_NEEDED);
-                return;
-            }
-        }
+        getMessages(input, UNIQUE_CHARS_NEEDED);
     } catch (err) {
         console.log(err);
     }
--- 6/part2/index.js
+++ 6/part2/index.js
@@ -1,13 +1,17 @@
 import fs from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from 'url';
+import { getMessages } from '../part1/index.js'
+
+const UNIQUE_CHARS_NEEDED = 14;
 
 function solve() {
     try {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = dirname(__filename);
 
-        const inputs = fs.readFileSync(__dirname + "../input.txt", 'utf8');
+        const input = fs.readFileSync(__dirname + "/../input.txt", 'utf8');
+        getMessages(input, UNIQUE_CHARS_NEEDED);
     } catch (err) {
         console.log(err);
     }
