da9a68a3fa368cf9d50867d3f850b473926be17d
Shane Vroegop <wizzy0309@hotmail.com>
2022-12-26 22:34:51 +1000

    Day 5 Part 1

--- 5/part1/index.js
+++ 5/part1/index.js
@@ -2,6 +2,17 @@ import fs from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from 'url';
 
+function getLines(boxes) {
+    return boxes.split("\n")
+        .map(line => {
+            return line
+                .split(new RegExp("(.{4}|.{3})")) // split each line into the columns
+                .filter(Boolean) // filter out any empty results
+                .filter(x => x.length > 1) // filter out the spaces
+                .map(x => x.trim());
+        }) // remove whitespace on boxes
+}
+
 function getStacks(lines) {
     const stacks = {};
     let stackCols = lines.pop();
@@ -18,31 +29,45 @@ function populateStacks(stacks, lines) {
     // the stack should be the index of the line + 1
     lines.forEach((line) => {
         line.forEach((box, index) => {
-            if(box !== "") {
+            if(box && box !== "") {
                 stacks[index + 1].unshift(box);
             }
         });
     });
 }
 
+function executeInstructions(instructions, stacks) {
+    let instructionsMatch = getInstructionFormat(instructions);
+    instructions
+        .split("\n")
+        .forEach(instruction => {
+            console.log(instruction);
+            const steps = instruction.match(instructionsMatch).map(Number);
+            if (steps) {
+                for (let i = 0; i < steps[0]; i++) {
+                    stacks[steps[2]].push(stacks[steps[1]].pop());
+                }
+            }
+        })
+}
+function getInstructionFormat() {
+    return new RegExp(/\d+/, "g");
+}
 function solve() {
     try {
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = dirname(__filename);
 
         const inputs = fs.readFileSync(__dirname + "/input.txt", 'utf8');
-        const [boxes, instructions] = inputs.split("\r\n\r\n");
-        let lines = boxes.split("\r\n");
-        lines = lines.map(line => line
-            .split(new RegExp("(.{3}(?= |\B))")) // split each line into the columns
-            .filter(Boolean) // filter out any empty results
-            .filter(x => x.length > 1) // filter out the spaces
-            .map(x => x.trim())) // remove whitespace on boxes
+        const [boxes, instructions] = inputs.split("\n\n");
+        let lines = getLines(boxes);
         const stacks = getStacks(lines);
-        populateStacks(stacks, lines)
-        console.log(stacks);
-        console.log(instructions);
-        console.log(lines);
+        populateStacks(stacks, lines);
+        executeInstructions(instructions, stacks);
+        console.table(stacks);
+        Object.values(stacks).forEach(stack => {
+            console.log(stack[stack.length - 1]);
+        });
     } catch (err) {
         console.log(err);
     }
