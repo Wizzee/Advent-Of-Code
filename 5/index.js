82c22d07ff6c70818a2dd181fbdb6e112d2c0940
Shane Vroegop <wizzy0309@hotmail.com>
2022-12-26 22:57:09 +1000

    Day 5 Part 2

--- 5/index.js
+++ 5/index.js
@@ -1,6 +1,6 @@
 import { exit } from "process";
 import { default as PartOne } from "./part1/index.js";
-// import { default as PartTwo } from "./part2/index.js";
+import { default as PartTwo } from "./part2/index.js";
 
 export function part(part) {
     switch(part) {
--- 5/part2/index.js
+++ 5/part2/index.js
@@ -1,40 +1,8 @@
 import fs from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from 'url';
+import { getLines, getStacks, getInstructionFormat, populateStacks } from "../part1/index.js"
 
-export function getLines(boxes) {
-    return boxes.split("\n")
-        .map(line => {
-            return line
-                .split(new RegExp("(.{4}|.{3})")) // split each line into the columns
-                .filter(Boolean) // filter out any empty results
-                .filter(x => x.length > 1) // filter out the spaces
-                .map(x => x.trim());
-        }) // remove whitespace on boxes
-}
-
-export function getStacks(lines) {
-    const stacks = {};
-    let stackCols = lines.pop();
-    stackCols.forEach(stack => {
-        if(stack !== "") {
-            stacks[stack] = [];
-        }
-    });
-    return stacks;
-}
-
-export function populateStacks(stacks, lines) {
-    // for each entry on a line, put a box on top of the stack
-    // the stack should be the index of the line + 1
-    lines.forEach((line) => {
-        line.forEach((box, index) => {
-            if(box && box !== "") {
-                stacks[index + 1].unshift(box);
-            }
-        });
-    });
-}
 
 function executeInstructions(instructions, stacks) {
     let instructionsMatch = getInstructionFormat(instructions);
@@ -44,14 +12,14 @@ function executeInstructions(instructions, stacks) {
             console.log(instruction);
             const steps = instruction.match(instructionsMatch).map(Number);
             if (steps) {
+                const boxesToMove = [];
                 for (let i = 0; i < steps[0]; i++) {
-                    stacks[steps[2]].push(stacks[steps[1]].pop());
+                    boxesToMove.push(stacks[steps[1]].pop());
                 }
+                boxesToMove.reverse();
+                stacks[steps[2]].push(...boxesToMove);
             }
         })
-}
-export function getInstructionFormat() {
-    return new RegExp(/\d+/, "g");
 }
 function solve() {
     try {
